34e25e884cd966e09b2c9a8e189b2d6e52fb9343
Aliaksandr Tsikhan <Aliaksandr_Tsikhan@epam.com>
2021-05-12 18:31:08 +0300

    Initial solution for the fourth module

--- third-module/src/config/routes.config.mjs
+++ third-module/src/config/routes.config.mjs
@@ -1,8 +1,16 @@
-export const ROUTES = {
-    GET_ALL: '/get-users',
-    GET_USER: '/get-user/:id',
-    CREATE_USER: '/create-user',
-    UPDATE_USER: '/update-user/:id',
-    REMOVE_USER: '/remove-user/:id',
-    GET_SUGGESTED_USERS: '/get-suggested-users'
+export const USER_ROUTES = {
+  GET_ALL: "/get-users",
+  GET_USER: "/get-user/:id",
+  CREATE_USER: "/create-user",
+  UPDATE_USER: "/update-user/:id",
+  REMOVE_USER: "/remove-user/:id",
+  GET_SUGGESTED_USERS: "/get-suggested-users",
+};
+
+export const GROUP_ROUTES = {
+  GET_ALL: "/get-groups",
+  GET_GROUP: "/get-group/:id",
+  CREATE_GROUP: "/create-group",
+  UPDATE_GROUP: "/update-group/:id",
+  REMOVE_GROUP: "/remove-group/:id",
 };
